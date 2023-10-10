866d9eddee0b037d8277b074e5213564a1046b2a
Blick05 <100322436+Blick05@users.noreply.github.com>
2023-10-10 15:41:12 +0000

    Modified Manage Databse tab

--- front-end/web.py
+++ front-end/web.py
@@ -28,27 +28,74 @@ theme = gr.themes.Base(
     button_primary_text_color_hover='#60a5fa',
     button_secondary_background_fill='#dbeafe'
 )
+
 #Manage Database catergory can be renamed
 def testss(category):
-    if "Tank Capacity" == category or "ISS Flight Plan Crew Nationality Lookup" == category or "ISS Flight Plan" == category:
-        one=gr.Textbox(interactive=True, visible=True)
-        two=gr.Textbox(interactive=True, visible=True)
-        three=gr.Textbox(interactive=True, visible=True)
+    if "Tank Capacity" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Tank Category")
+        two=gr.Textbox(interactive=True, visible=True, label="Tank Capacity")
+        three=gr.Textbox(interactive=True, visible=True, label="Units")
         four = gr.Textbox(interactive=True, visible=False)
         five = gr.Textbox(interactive=True, visible=False)
-    elif "US Weekly Consumable Water Summary" == category:
-        one=gr.Textbox(interactive=True, visible=True)
-        two=gr.Textbox(interactive=True, visible=True)
-        three=gr.Textbox(interactive=True, visible=True)
-        four = gr.Textbox(interactive=True, visible=True)
-        five = gr.Textbox(interactive=True, visible=True)
-    elif "Thresholds/Limits" == category or "RS Weekly Consumable Water Summary" == category:
-        one=gr.Textbox(interactive=True, visible=True)
-        two=gr.Textbox(interactive=True, visible=True)
-        three=gr.Textbox(interactive=True, visible=True)
-        four = gr.Textbox(interactive=True, visible=True)
+        six = gr.Textbox(interactive=True, visible=False)
+        seven = gr.Textbox(interactive=True, visible=False)
+    elif "ISS Flight Plan" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Event Name")
+        two=gr.Textbox(interactive=True, visible=True, label="Port Name")
+        three=gr.Textbox(interactive=True, visible=True, label="Vehicle Type")
+        four = gr.Textbox(interactive=True, visible=True, label="Date")
+        five = gr.Textbox(interactive=True, visible=True, label="Eva Type")
+        six = gr.Textbox(interactive=True, visible=True, label="Eva Accuracy")
+        seven = gr.Textbox(interactive=True, visible=True, label="Event")
+    elif "ISS Flight Plan Crew Nationality Lookup" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Nationality")
+        two=gr.Textbox(interactive=True, visible=True, label="USOS Crew")
+        three=gr.Textbox(interactive=True, visible=True, label="RSA Crew")
+        four = gr.Textbox(interactive=True, visible=False)
         five = gr.Textbox(interactive=True, visible=False)
-    return(one,two,three,four,five)
+        six = gr.Textbox(interactive=True, visible=False)
+        seven = gr.Textbox(interactive=True, visible=False)
+    elif "Rates Definition" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Rate Category")
+        two=gr.Textbox(interactive=True, visible=True, label="Type")
+        three=gr.Textbox(interactive=True, visible=True, label="Affected Consumable")
+        four = gr.Textbox(interactive=True, visible=True, label="Rate")
+        five = gr.Textbox(interactive=True, visible=True, label="Units")
+        six = gr.Textbox(interactive=True, visible=True, label="Efficiency")
+        seven = gr.Textbox(interactive=True, visible=False)
+    elif "US Weekly Consumable Water Summary" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Date")
+        two=gr.Textbox(interactive=True, visible=True, label="Corrected Potable")
+        three=gr.Textbox(interactive=True, visible=True, label="Corrected Technical")
+        four = gr.Textbox(interactive=True, visible=True, label="Corrected Total")
+        five = gr.Textbox(interactive=True, visible=True, label="Resupply Potable")
+        six = gr.Textbox(interactive=True, visible=True, label="Resupply Technical")
+        seven = gr.Textbox(interactive=True, visible=True, label="Corrected Predicted")
+    elif "IMS Consumables" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Category Name")
+        two=gr.Textbox(interactive=True, visible=True, label="Category ID")
+        three=gr.Textbox(interactive=True, visible=True, label="Module Name")
+        four = gr.Textbox(interactive=True, visible=True, label="Module ID")
+        five = gr.Textbox(interactive=True, visible=True, label="Unique Cat Mod ID")
+        six = gr.Textbox(interactive=True, visible=False)
+        seven = gr.Textbox(interactive=True, visible=False)
+    elif "Thresholds/Limits" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Threshold Category")
+        two=gr.Textbox(interactive=True, visible=True, label="Threshold Value")
+        three=gr.Textbox(interactive=True, visible=True, label="Threshold Owner")
+        four = gr.Textbox(interactive=True, visible=True, label="Units")
+        five = gr.Textbox(interactive=True, visible=False)
+        six = gr.Textbox(interactive=True, visible=False)
+        seven = gr.Textbox(interactive=True, visible=False)
+    elif "RS Weekly Consumable Water Summary" == category:
+        one=gr.Textbox(interactive=True, visible=True, label="Report Date")
+        two=gr.Textbox(interactive=True, visible=True, label="Remain. Potable(Liters)")
+        three=gr.Textbox(interactive=True, visible=True, label="Rmain Technical(Liters)")
+        four = gr.Textbox(interactive=True, visible=True, label="Remain Rodnik(Liters)")
+        five = gr.Textbox(interactive=True, visible=False)
+        six = gr.Textbox(interactive=True, visible=False)
+        seven = gr.Textbox(interactive=True, visible=False)
+    return(one,two,three,four,five,six,seven)
 
 #does authentication with the login cookies must be enabled in your browser to make this happen
 def authentication(username,password):
@@ -72,7 +119,7 @@ with gr.Blocks(theme=theme, title="Adventures") as mockup:
             csv3 = gr.File(file_types = [".csv"], label = "US Weekly Consumable Water Summary", interactive = True)
             csv4 = gr.File(file_types = [".csv"], label = "RS Weekly Consumable Water Summary", interactive = True)
         with gr.Row():
-            csv5 = gr.File(file_types = [".csv"], label = "IMS Consumables Category Lookup", interactive = True)
+            csv5 = gr.File(file_types = [".csv"], label = "IMS Consumables", interactive = True)
             csv6 = gr.File(file_types = [".csv"], label = "ISS Flight Plan Crew", interactive = True)
             csv7 = gr.File(file_types = [".csv"], label = "Rates Definition", interactive = True)
             csv8 = gr.File(file_types = [".csv"], label = "Thresholds/Limits", interactive = True)
@@ -84,7 +131,7 @@ with gr.Blocks(theme=theme, title="Adventures") as mockup:
     with gr.Tab("Manage Database"):
         manage = gr.Label(value="Manage Database")
         mdCategoryDropdown = gr.Dropdown(choices = ["Tank Capacity", "US/RS Weekly Consumable Gas Summary", "US Weekly Consumable Water Summary",
-                                                    "RS Weekly Consumable Water Summary", "IMS Consumables Category Lookup", "ISS Flight Plan Crew",
+                                                    "RS Weekly Consumable Water Summary", "IMS Consumables", "ISS Flight Plan Crew",
                                                       "Rates Definition", "Thresholds/Limits","ISS Flight Plan Crew Nationality Lookup", 
                                                       "Inventory Management System Consumables", "ISS Flight Plan", "Stored Items Only Inventory Management System Consumables"])
         with gr.Row():# more can be added 
@@ -93,21 +140,29 @@ with gr.Blocks(theme=theme, title="Adventures") as mockup:
             three = gr.Textbox(interactive=True, visible=False)
             four = gr.Textbox(interactive=True, visible=False)
             five = gr.Textbox(interactive=True, visible=False)
-            mdCategoryDropdown.input(fn=testss, inputs=[mdCategoryDropdown], outputs=[one,two,three,four,five])
+            six = gr.Textbox(interactive=True, visible=False)
+            seven = gr.Textbox(interactive=True, visible=False)
+            mdCategoryDropdown.input(fn=testss, inputs=[mdCategoryDropdown], outputs=[one,two,three,four,five,six,seven])
         with gr.Row():
             ones = gr.Textbox(interactive=True, visible=False)
             twos = gr.Textbox(interactive=True, visible=False)
             threes = gr.Textbox(interactive=True, visible=False)
             fours = gr.Textbox(interactive=True, visible=False)
             fives = gr.Textbox(interactive=True, visible=False)
-            mdCategoryDropdown.input(fn=testss, inputs=[mdCategoryDropdown], outputs=[ones,twos,threes,fours,fives])
+            sixs = gr.Textbox(interactive=True, visible=False)
+            sevens = gr.Textbox(interactive=True, visible=False)
+            mdCategoryDropdown.input(fn=testss, inputs=[mdCategoryDropdown], outputs=[ones,twos,threes,fours,fives,sixs,sevens])
         with gr.Row():
             oness = gr.Textbox(interactive=True, visible=False)
             twoss = gr.Textbox(interactive=True, visible=False)
             threess = gr.Textbox(interactive=True, visible=False)
             fourss = gr.Textbox(interactive=True, visible=False)
             fivess = gr.Textbox(interactive=True, visible=False)
-            mdCategoryDropdown.input(fn=testss, inputs=[mdCategoryDropdown], outputs=[oness,twoss,threess,fourss,fivess])
+            sixss = gr.Textbox(interactive=True, visible=False)
+            sevenss = gr.Textbox(interactive=True, visible=False)
+            mdCategoryDropdown.input(fn=testss, inputs=[mdCategoryDropdown], outputs=[oness,twoss,threess,fourss,fivess,sixss,sevenss])
+        submitChanges = gr.Button(value="Submit")
+        #gr.submitChanges.click(fn=,input)
             
 
     with gr.Tab("View Analyses"):
