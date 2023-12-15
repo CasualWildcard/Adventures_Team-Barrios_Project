33bf15699a85341a675fd216df86df53a37a0145
Jacob Elliott <jelliott22@angelo.edu>
2023-12-14 23:30:43 -0600

    fixed install.bat

--- install.bat
+++ install.bat
@@ -1,16 +1,48 @@
 @echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------  
 pip install gradio==3.50.2
 pip install plotly
 
-mkdir C:\ProgramFiles\AdventureTeamBarrios
+mkdir "C:\Program Files\AdventureTeamBarrios"
 
-move * C:\ProgramFiles\AdventureTeamBarrios
+xcopy /s /i %CD%\* "C:\Program Files\AdventureTeamBarrios"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%USERPROFILE%\Desktop\MyShortcut.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "C:\Users\Public\Desktop\MyShortcut.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "C:\ProgramFiles\AdventureTeamBarrios\front-end\web.py" >> CreateShortcut.vbs
+echo oLink.TargetPath = "C:\Program Files\AdventureTeamBarrios\front-end\web.py" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
+
+del "C:\Program Files\AdventureTeamBarrios\install.bat"
+pause
